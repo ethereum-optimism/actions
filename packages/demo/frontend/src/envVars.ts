dd48a5dd97946e75b8c4d49132bd9fa8a9f45f4b
its-everdred <kevinweaver2@gmail.com>
2025-07-24 09:42:01 -0700

    Update circleci steps, add npm publish

--- packages/demo/frontend/src/envVars.ts
+++ packages/demo/frontend/src/envVars.ts
@@ -6,7 +6,7 @@ const envVarSchema = z.object({
     .url()
     .default(
       import.meta.env.MODE === 'production'
-        ? 'https://dev-verbs-service.optimism.io/'
+        ? 'https://dev-verbs-service.optimism.io'
         : 'http://localhost:3000',
     )
     .describe('Base URL for the verbs service API'),
