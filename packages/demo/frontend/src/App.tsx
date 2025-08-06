424f60e038447d7e69e6b272ae7f45d4d92b597a
tre <tremarkley@gmail.com>
2025-08-06 16:06:54 +0100

    remove venmo

--- packages/demo/frontend/src/App.tsx
+++ packages/demo/frontend/src/App.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Terminal from './components/Terminal'
 import Home from './components/Home'
 import Revolut from './components/Revolut'
-import Venmo from './components/Venmo'
 
 function App() {
   return (
@@ -12,7 +11,6 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/demo" element={<Terminal />} />
           <Route path="/revolut" element={<Revolut />} />
-          <Route path="/venmo" element={<Venmo />} />
         </Routes>
       </div>
     </Router>
