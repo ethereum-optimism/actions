7ff28e6ac29fe4b2d534524bfdaa1f4b85673557
Tarun Khasnavis <tarunkhasnavis@Taruns-MacBook-Pro-2.local>
2025-08-06 13:52:31 +0100

    adding venmo page

--- packages/demo/frontend/src/App.tsx
+++ packages/demo/frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Terminal from './components/Terminal'
 import Home from './components/Home'
+import Venmo from './components/Venmo'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <div className="w-full h-screen bg-terminal-bg">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/venmo" element={<Venmo />} />
           <Route path="/demo" element={<Terminal />} />
         </Routes>
       </div>
