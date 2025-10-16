d4e2cbab79783efe65e460c3db56b5d3baa221cc
its-everdred <kevinweaver2@gmail.com>
2025-10-16 11:37:57 -0700

    Update SDK description

--- packages/demo/frontend/src/components/home/Overview.tsx
+++ packages/demo/frontend/src/components/home/Overview.tsx
@@ -35,10 +35,11 @@ function Overview() {
         <h2 className="text-3xl font-medium text-gray-300 mb-4">Overview</h2>
         <div className="h-px bg-gradient-to-r from-gray-600 via-gray-500 to-transparent mb-4"></div>
         <p className="text-gray-300 mb-4">
-          Actions is an open source TypeScript SDK for onchain actions:{' '}
-          <strong>Lend</strong>, <strong>Borrow</strong>, <strong>Swap</strong>,{' '}
-          <strong>Pay</strong>, without managing complex infrastructure or
-          custody.
+          Actions is an open source TypeScript SDK for letting your users easily
+          perform onchain actions: <strong>Lend</strong>,{' '}
+          <strong>Borrow</strong>, <strong>Swap</strong>, <strong>Pay</strong>,
+          without managing complex infrastructure or custody. Integrate DeFi with
+          a single dependency.
         </p>
       </div>
       <div className="max-w-4xl mx-auto">
