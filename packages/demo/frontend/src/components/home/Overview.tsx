691ecb3017224d8c9cfca869f888a88acdbe72f1
its-everdred <kevinweaver2@gmail.com>
2026-03-19 11:21:57 -0700

    create white velodrome logo, restore velo on home page

--- packages/demo/frontend/src/components/home/Overview.tsx
+++ packages/demo/frontend/src/components/home/Overview.tsx
@@ -74,7 +74,7 @@ const receipt = wallet.borrow.openPosition({
       'Enable onchain trading between configurable protocols and assets.',
     images: [
       { src: '/uniswap-logo-white.svg', link: 'https://uniswap.org/' },
-      { src: '/aerodrome-logo.svg', link: 'https://aerodrome.finance/' },
+      { src: '/velodrome-logo-white.svg', link: 'https://velodrome.finance/' },
     ],
     imageLabel: 'Supports swap providers:',
     code: `// Swap between tokens
