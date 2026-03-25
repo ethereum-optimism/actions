e5bc09876d5ff9d59404fa3967e165e33ea9c5d9
its-everdred <kevinweaver2@gmail.com>
2026-03-24 22:12:17 -0700

    allow async factory create return type

--- packages/sdk/src/wallet/core/providers/hosted/types/index.ts
+++ packages/sdk/src/wallet/core/providers/hosted/types/index.ts
@@ -56,7 +56,7 @@ export interface HostedProviderFactory<
 > {
   type: TType
   validateOptions(options: unknown): options is TOptions
-  create(deps: HostedProviderDeps, options: TOptions): TInstance
+  create(deps: HostedProviderDeps, options: TOptions): TInstance | Promise<TInstance>
 }
 
 /**
